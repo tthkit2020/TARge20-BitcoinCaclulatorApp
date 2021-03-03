9415084dc079c7ce36183be9be6365d0a1064c1e
Julia Aasmaa <my_e_10@hotmail.com>
2021-03-03 10:18:26 +0200

    Add BitCoin Rates In EUR

---
+++ BitCoinApp/BitCoinRates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCoinApp
+{
+    public class BitCoinRates
+    {
+        public bpi bpi { get; set; }
+    }
+
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+
+    }
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+
+
+}
